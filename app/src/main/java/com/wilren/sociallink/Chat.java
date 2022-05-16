ceb33e061edcfc9c888511ef94e853a2cc65d890
Enrique <fpemlktz@gmail.com>
2022-05-16 12:51:46 +0200

    Username

--- app/src/main/java/com/wilren/sociallink/Chat.java
+++ app/src/main/java/com/wilren/sociallink/Chat.java
@@ -1,17 +1,25 @@
 package com.wilren.sociallink;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -22,8 +30,9 @@ import java.util.Date;
 public class Chat extends AppCompatActivity {
 
     private RecyclerView rvMensajes;
+    private TextView NameUser;
     private EditText etMensaje;
-    private ImageButton btnSend;
+    private ImageButton btnSend,btn;
     private AdapterChatAuth AdaptadorChats;
     private final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -66,6 +75,15 @@ public class Chat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         setComponents();
+        setUserDatachat();
+
+
+         /*btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                // setProfileSettings();
+             }
+         });*/
     }
 
     @Override
@@ -77,5 +95,33 @@ public class Chat extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         AdaptadorChats.stopListening();
+    }
+
+    /*public void setProfileSettings(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName("Jane Q. User")
+                .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("TAG", "User profile updated.");
+                        }
+                    }
+                });
+    }*/
+
+    public void setUserDatachat(){
+        NameUser=findViewById(R.id.userNameChat);
+        btn= findViewById(R.id.userIdChat);
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        NameUser.setText(user.getEmail());
+        //btn.setImageURI();
+
     }
 }
